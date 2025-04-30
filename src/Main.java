eb0e3a0154b51425bc825b0fa877445806aac21b
mayconslv <mayconslv.contato@gmail.com>
2025-04-30 11:05:47 -0300

    feat: testing methods in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import api.ExchangeRateApiClient;
+import api.dto.ExchangeRateApiResponse;
+import model.Currency;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual moeda você quer usar como base? Use o formato BRL, USD...");
+        String baseCurrency = scanner.next();
+
+        System.out.println("Qual o valor você deseja converter?");
+        double valueToConvert = scanner.nextDouble();
+
+        System.out.println("Para qual moeda você quer converter? Use o formato BRL, USD...");
+        String currencyToConvert = scanner.next();
+
+        try {
+            ExchangeRateApiClient api = new ExchangeRateApiClient();
+            ExchangeRateApiResponse response = api.request(baseCurrency);
+            
+
+            Currency currency = new Currency();
+            currency.converter(valueToConvert, response.conversion_rates().get(currencyToConvert), baseCurrency);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
