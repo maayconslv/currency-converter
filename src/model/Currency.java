f41fdc96ee3580de491cf200431b6a591c6973fd
mayconslv <mayconslv.contato@gmail.com>
2025-04-30 11:05:10 -0300

    feat: add Currency entity to convert the value to the currency

--- src/model/Currency.java
+++ src/model/Currency.java
@@ -1,4 +1,12 @@
-package model.dto;
+package model;
 
 public class Currency {
+    private int currency;
+    private String baseCode;
+
+    public void converter(double value, double multiplier, String currency) {
+        double multipliedValue = value * multiplier;
+
+        System.out.println("R$" + value + " convertido para " + currency +  "é " + multipliedValue);
+    }
 }
