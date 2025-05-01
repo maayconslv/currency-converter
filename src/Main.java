f69fb841b3baf43b915231f2cf8b122e604d94ba
mayconslv <mayconslv.contato@gmail.com>
2025-05-01 11:30:39 -0300

    refactor: add methods to refactor the main method

--- src/Main.java
+++ src/Main.java
@@ -6,36 +6,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int hasLoop = 0;
         Scanner scanner = new Scanner(System.in);
+        int hasLoop = 0;
 
         do {
-            System.out.println("Qual moeda você quer usar como base? Use o formato BRL, USD...");
-            String baseCurrency = scanner.next();
-
-            System.out.println("Qual o valor você deseja converter?");
-            double valueToConvert = scanner.nextDouble();
-
-            System.out.println("Para qual moeda você quer converter? Use o formato BRL, USD...");
-            String currencyToConvert = scanner.next();
-
-            try {
-                ExchangeRateApiClient api = new ExchangeRateApiClient();
-                ExchangeRateApiResponse response = api.request(baseCurrency);
-
-
-                Currency currency = new Currency();
-                currency.converter(valueToConvert, response.conversion_rates().get(currencyToConvert), baseCurrency);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-
-            System.out.println("Digite 0 para continuar e 1 para sair");
-            hasLoop = scanner.nextInt();
+            executarConversao(scanner);
+            hasLoop = askIfContinues(scanner);
         } while (hasLoop == 0);
 
         System.out.println("Programa encerrado.");
         scanner.close();
+    }
 
+    private static void executarConversao(Scanner scanner) {
+        System.out.println("Qual moeda você quer usar como base? Use o formato BRL, USD...");
+        String baseCurrency = scanner.next();
+
+        System.out.println("Qual o valor você deseja converter?");
+        double valueToConvert = scanner.nextDouble();
+
+        System.out.println("Para qual moeda você quer converter? Use o formato BRL, USD...");
+        String currencyToConvert = scanner.next();
+
+        try {
+            ExchangeRateApiClient api = new ExchangeRateApiClient();
+            ExchangeRateApiResponse response = api.request(baseCurrency);
+
+            Currency currency = new Currency();
+            currency.converter(valueToConvert, response.conversion_rates().get(currencyToConvert), baseCurrency);
+        } catch (Exception e) {
+            System.out.println("Erro ao realizar a conversão: " + e.getMessage());
+        }
+    }
+
+    private static int askIfContinues(Scanner scanner) {
+        System.out.println("Digite 0 para continuar e 1 para sair");
+        return scanner.nextInt();
     }
 }
